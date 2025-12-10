f9a4a172d743a83e4e847b470c31bbb7d6fe1bac
joao victor <jv224063@gmail.com>
2025-12-10 16:08:14 -0300

    add to in Link

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,9 +9,9 @@ function    Home () {
             <nav>
                 <ul className="py-4 bg-greenSub text-white ">
                     <li className="grid grid-cols-4 w-150 mx-auto place-items-center place-self-end transition-color duration-100 rounded-lg">
-                        <Link className="p-2 rounded-md font-bold hover:bg-greenNav" href="/">Home</Link>
-                        <Link className="p-2 rounded-md font-bold hover:bg-greenNav" href="/Planos">Planos</Link>
-                        <Link className="p-2 rounded-md font-bold hover:bg-greenNav" href="/Dashboard">Dashboard</Link>
+                        <Link className="p-2 rounded-md font-bold hover:bg-greenNav" to="/">Home</Link>
+                        <Link className="p-2 rounded-md font-bold hover:bg-greenNav" to="/Planos">Planos</Link>
+                        <Link className="p-2 rounded-md font-bold hover:bg-greenNav" to="/Dashboard">Dashboard</Link>
                         <button>
                             <a className="p-2 rounded-md font-bold hover:bg-greenNav hover:border " href="/Dashboard">Entrar</a>
                         </button>
