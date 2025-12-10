00868c4644eef812f8130562f10d5cb2eb3775c6
joao victor <jv224063@gmail.com>
2025-12-10 15:54:49 -0300

    commit hash test 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
-import { HashRouter} from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Planos from "./Pages/Planos";
 import Dashboard from "./Pages/Dashboard";
-import "./App.css";
-
-
 
 function App() {
   return (
     <HashRouter>
       <Routes>
-
-        {/* Página inicial */}
         <Route path="/" element={<Home />} />
-        
-
-        {/* Outras páginas */}
         <Route path="/Planos" element={<Planos />} />
         <Route path="/Dashboard" element={<Dashboard />} />
-
       </Routes>
     </HashRouter>
   );
