03557a7ec249734616feef65ba6c13124d216c1d
joao victor <jv224063@gmail.com>
2025-12-10 16:05:17 -0300

    add Link

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import react from "react"
 import Main from "./ComponentsHome/Main"
+import { Link } from "react-router-dom";
 
 function    Home () {
     return(
@@ -8,9 +9,9 @@ function    Home () {
             <nav>
                 <ul className="py-4 bg-greenSub text-white ">
                     <li className="grid grid-cols-4 w-150 mx-auto place-items-center place-self-end transition-color duration-100 rounded-lg">
-                        <link className="p-2 rounded-md font-bold hover:bg-greenNav" href="/">Home</link>
-                        <link className="p-2 rounded-md font-bold hover:bg-greenNav" href="/Planos">Planos</link>
-                        <link className="p-2 rounded-md font-bold hover:bg-greenNav" href="/Dashboard">Dashboard</link>
+                        <Link className="p-2 rounded-md font-bold hover:bg-greenNav" href="/">Home</Link>
+                        <Link className="p-2 rounded-md font-bold hover:bg-greenNav" href="/Planos">Planos</Link>
+                        <Link className="p-2 rounded-md font-bold hover:bg-greenNav" href="/Dashboard">Dashboard</Link>
                         <button>
                             <a className="p-2 rounded-md font-bold hover:bg-greenNav hover:border " href="/Dashboard">Entrar</a>
                         </button>
